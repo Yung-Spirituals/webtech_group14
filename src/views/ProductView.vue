1641882891d3507718078cdbec88fa145cc54669
Øystein Grande <oystgran@stud.ntnu.no>
2022-05-21 14:35:59 +0200

    added some imports to productview

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,7 +1,18 @@
 <script>
 import Header from "@/components/header.vue";
 import Footer from "@/components/footer.vue";
+import AppButton from "@/components/appbutton.vue";
 import ProductContent from "@/components/ProductContent.vue";
+
+export default {
+  components: {
+    AppButton,
+    GlobalStyles,
+    Header,
+    Footer,
+    ProductContent,
+  },
+};
 </script>
 
 <template>
