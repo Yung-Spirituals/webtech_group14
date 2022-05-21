0fc38efc434288e66dc060ff967ae464632fe9e8
Øystein Grande <oystgran@stud.ntnu.no>
2022-05-21 14:18:12 +0200

    Converted Product Pages

---
+++ src/views/ProductView.vue
@@ -0,0 +1,13 @@
+<script>
+import Header from "@/components/header.vue";
+import Footer from "@/components/footer.vue";
+import ProductContent from "@/components/ProductContent.vue";
+</script>
+
+<template>
+  <Header></Header>
+  <ProductContent></ProductContent>
+  <Footer></Footer>
+</template>
+
+<style scoped src="../productStyle.css"></style>
