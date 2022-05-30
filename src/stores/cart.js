4130397388e1435467be09df924e43e7a3a5cb28
Aron Selven <aronselven@gmail.com>
2022-05-30 22:10:01 +0200

    minor changes in preperation to move everything to server

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,58 +1,78 @@
-import { defineStore } from 'pinia'
-import axios from 'axios'
+import {defineStore} from 'pinia';
+import {getCookie} from './authentication';
+import {getAuthenticatedUser} from './authentication';
 
-/*
-interface Cart {
-    cid: string
-    products: Array<{ id: number, qty: number }>
-}
-
-interface State {
-    cart: Cart | []
-}
-
-export const useCartStore = defineStore({
-    id: cart,
-    state: () => ({ cart: [] } as State),
-    actions: {
-
-    }
-}
-*/
 export const useCartStore = defineStore({
     id: 'cart',
     state: () => ({
-        cart: [],
-        /*
-        uniqueProducts: []
-        */
+        carts:[]
     }),
-
-    getters: {
-        UniqueProducts: (state) => state.uniqueProducts,
-    },
     actions: {
-        addToCart(product) {
-            this.cart.push(product);
-            /*
-            if (productAmount[cart[i].id]) {
-                productAmount[cart[i].id]++;
+        addProductToCart(item){
+            if (this.isInCart(item)){
+                this.updateProductInCart(item.id, item.quantity + 1);
             } else {
-                productAmount[cart[i].id] = 1;
+                let cartItem = {
+                    "id": item.id,
+                    "quantity": 1,
+                    "product": item
+                }
+                this.carts.push(cartItem);
             }
-            if (!uniqueProductIDs.includes(cart[i].id)) {
-                uniqueProductIDs.push(cart[i].id);
-                uniqueProducts.push(cart[i]);
-            }
-            */
         },
-
-        removeFromCart(id) {
-            const index = this.cart.findIndex((product) => {
-                return product.id === id;
-            });
-            this.cart.splice(index, 1);
-            console.log(this.cart);
+        updateProductInCart(id, product, newQuantity){
+            if (id===null && newQuantity===null) {
+            } else {
+                const index = this.findIndexById(id);
+                if (index !== -1) {
+                    this.carts[index] = {
+                        "id": id,
+                        "quantity": newQuantity,
+                        "product": product
+                    }
+                }
+            }
+        },
+        removeProductFromCart(id) {
+            const index = this.findIndexById(id);
+            if (index === -1) {
+            } else {
+                this.carts.splice(index, 1);
+            }
+        },
+        findIndexById(id){
+            return this.carts.findIndex((item) => item.id === id);
+        },
+        isInCart(item) {
+            if (this.carts === null){return false}
+            let inCart = false;
+            this.carts.forEach(value => function (){
+                if (value === item){
+                    inCart = true;
+                }
+            })
+            return inCart;
+        },
+        emptyCart(){
+            this.carts=[];
+            fetch("https://gr14.appdev.cloudns.ph/api/cart", {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': 'bearer: ' + getCookie('jwt')
+                }
+            })
+        },
+        async getCart(){
+            const response = await fetch("https://gr14.appdev.cloudns.ph/api/cart", {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'bearer: ' + getCookie('jwt')
+                }
+            })
+            this.carts = await response.json();
         }
+    },
+    getters: {
+
     }
 })
--- src/views/ShoppingCartView.vue
+++ src/views/ShoppingCartView.vue
@@ -1,161 +1,68 @@
 <template>
-  <div class="content">
-    <h1 class="title">Shopping Cart</h1>
-    {{ cart }}
-    <section id="cart-container" class="container my-5">
-      <table width="100%">
-        <thead>
-          <tr>
-            <td>Remove</td>
-            <td>Image</td>
-            <td>Product</td>
-            <td>Price</td>
-            <td>Quantity</td>
-            <td>Total</td>
-            <td>Total</td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="product in uniqueProducts"
-            :key="product.id"
-            class="product"
-          >
-            <td>
-              <span
-                id="deleteButton"
-                class="material-symbols-rounded"
-                @click="removeFromCart(product.id)"
-              >
-                delete
-              </span>
-            </td>
-            <td><img :src="product.image_path" width="30" height="30" /></td>
-            <td>
-              <h5>{{ product.name }}</h5>
-            </td>
-            <td>{{ product.price }}</td>
-            <td>
-              {{ productAmount[product.id] }}
-            </td>
-            <td>{{ productAmount[product.id] * product.price }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </section>
+  <div id="content">
+    <h1>Shopping Cart</h1>
+    <table>
+      <thead>
+      <tr>
+        <th>Product</th>
+        <th>Price</th>
+        <th>Quantity</th>
+        <th>Subtotal</th>
+        <th>Remove</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="cart in carts" :key="cart.id" class="cart">
+        <!------- Product -------->
+        <td>
+          <div>
+            <img :src="cart.product.image_path" alt=""/>
+            <label>{{ cart.product.name }}</label>
+          </div>
+        </td>
+        <td>
+          {{ cart.product.price }}
+        </td>
+        <td>
+          <input type="number" value="1" />
+        </td>
+        <td>
+          {{cart.quantity}}
+        </td>
+        <td>
+          <button class="delete-button"><span class="material-symbols-rounded">delete</span></button>
+        </td>
+      </tr>
+      </tbody>
+      <tfoot>
+      <tr>
+      <td>
+        sum
+      </td>
+      <td>
+        99999$
+      </td>
+      </tr>
+      </tfoot>
+    </table>
   </div>
 </template>
 
 <script setup>
+import { storeToRefs } from 'pinia';
 import { useCartStore } from "@/stores/cart";
-const cartStore = useCartStore();
-const cart = cartStore.cart;
-const productAmount = {};
-const uniqueProductIDs = [];
-const uniqueProducts = [];
-
-for (var i = 0; i < cart.length; i++) {
-  if (productAmount[cart[i].id]) {
-    productAmount[cart[i].id]++;
-  } else {
-    productAmount[cart[i].id] = 1;
-  }
-  if (!uniqueProductIDs.includes(cart[i].id)) {
-    uniqueProductIDs.push(cart[i].id);
-    uniqueProducts.push(cart[i]);
-  }
-}
-
-function removeFromCart(id) {
-  cartStore.removeFromCart(id);
-  console.log("Hei");
-}
+const store = useCartStore();
+store.getCart();
+const { carts } = storeToRefs(store);
 </script>
 
-<style scoped>
-.content {
-  display: flex;
-  flex-direction: column;
-  padding: 2rem;
-}
-.title {
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-}
-.table {
+<style>
+#content {
   display: grid;
-  grid-template-areas:
-    "productTitle amountTitle priceTitle totalTitle deleteTitle"
-    "product      amount      price       total      delete"
-    "checkout     checkout    checkout    checkout   checkout";
-  grid-template-columns: 100px 1fr 100px;
-  grid-template-columns: 2fr 0.5fr 0.5fr 0.5fr 0.5fr;
+  grid-template-columns: 1fr;
+  grid-template-rows: 30rem 1fr;
 }
-
-#cart-container table {
-  border-collapse: collapse;
-  width: 100%;
-  table-layout: fixed;
-}
-
-#cart-container table thead {
-  font-weight: 700;
-}
-
-#cart-container table thead td {
-  background: #55d6aa;
-  border: none;
-  padding: 6px;
-}
-
-#cart-container table td {
-  border: 1px solid #b6b3b3;
-  text-align: center;
-}
-
-#deleteButton:hover {
-  cursor: pointer;
-  user-select: none;
-}
-
-#productTitle {
-  grid-area: productTitle;
-}
-#amountTitle {
-  grid-area: amountTitle;
-}
-#priceTitle {
-  grid-area: priceTitle;
-}
-#totalTitle {
-  grid-area: totalTitle;
-}
-#deleteTitle {
-  grid-area: deleteTitle;
-}
-
-#productColumn {
-  grid-area: product;
-}
-#amountColumn {
-  grid-area: amount;
-}
-#priceColumn {
-  grid-area: price;
-}
-#totalColumn {
-  grid-area: total;
-}
-#deleteColumn {
-  grid-area: delete;
-}
-
-.table {
-  grid-area: table;
-  display: grid;
-}
-h1 {
-  grid-area: title;
+h1{
+  grid-area: 1/1/2/2;
 }
 </style>
